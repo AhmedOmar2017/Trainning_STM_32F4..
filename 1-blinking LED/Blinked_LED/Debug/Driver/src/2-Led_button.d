be2854ea1aaf84dabd5951171548a991c0136b89
Ahmed Omar <eng.ahmed.omar2017@gmail.com>
2024-02-12 23:58:08 +0200

    led_button

--- 1-blinking LED/Blinked_LED/Debug/Driver/src/2-Led_button.d
+++ 1-blinking LED/Blinked_LED/Debug/Driver/src/2-Led_button.d
@@ -1,4 +1,4 @@
-Driver/src/001led taggle.o: ../Driver/src/001led\ taggle.c \
+Driver/src/2-Led_button.o: ../Driver/src/2-Led_button.c \
  D:/courses/Done/Embedded\ &\ Electronics/Mastering\ Microcontroller\ and\ Embedded\ Driver\ Development/Drivers/code_updates/1-blinking\ LED/Blinked_LED/Driver/inc/HAL.h \
  D:/courses/Done/Embedded\ &\ Electronics/Mastering\ Microcontroller\ and\ Embedded\ Driver\ Development/Drivers/code_updates/1-blinking\ LED/Blinked_LED/Driver/inc/Mcal.h \
  D:/courses/Done/Embedded\ &\ Electronics/Mastering\ Microcontroller\ and\ Embedded\ Driver\ Development/Drivers/code_updates/1-blinking\ LED/Blinked_LED/Driver/inc/macros.h \
--- 1-blinking LED/Blinked_LED/Debug/Driver/src/subdir.mk
+++ 1-blinking LED/Blinked_LED/Debug/Driver/src/subdir.mk
@@ -5,34 +5,32 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Driver/src/001led\ taggle.c \
+../Driver/src/2-Led_button.c \
 ../Driver/src/HAL.c \
 ../Driver/src/syscalls.c \
 ../Driver/src/sysmem.c 
 
 OBJS += \
-./Driver/src/001led\ taggle.o \
+./Driver/src/2-Led_button.o \
 ./Driver/src/HAL.o \
 ./Driver/src/syscalls.o \
 ./Driver/src/sysmem.o 
 
 C_DEPS += \
-./Driver/src/001led\ taggle.d \
+./Driver/src/2-Led_button.d \
 ./Driver/src/HAL.d \
 ./Driver/src/syscalls.d \
 ./Driver/src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Driver/src/001led\ taggle.o: ../Driver/src/001led\ taggle.c Driver/src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F401RCTx -c -I../Inc -I"D:/courses/Done/Embedded & Electronics/Mastering Microcontroller and Embedded Driver Development/Drivers/code_updates/1-blinking LED/Blinked_LED/Driver/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Driver/src/001led taggle.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 Driver/src/%.o Driver/src/%.su: ../Driver/src/%.c Driver/src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F4 -DSTM32F401RCTx -c -I../Inc -I"D:/courses/Done/Embedded & Electronics/Mastering Microcontroller and Embedded Driver Development/Drivers/code_updates/1-blinking LED/Blinked_LED/Driver/inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
 clean: clean-Driver-2f-src
 
 clean-Driver-2f-src:
-	-$(RM) ./Driver/src/001led\ taggle.d ./Driver/src/001led\ taggle.o ./Driver/src/001led\ taggle.su ./Driver/src/HAL.d ./Driver/src/HAL.o ./Driver/src/HAL.su ./Driver/src/syscalls.d ./Driver/src/syscalls.o ./Driver/src/syscalls.su ./Driver/src/sysmem.d ./Driver/src/sysmem.o ./Driver/src/sysmem.su
+	-$(RM) ./Driver/src/2-Led_button.d ./Driver/src/2-Led_button.o ./Driver/src/2-Led_button.su ./Driver/src/HAL.d ./Driver/src/HAL.o ./Driver/src/HAL.su ./Driver/src/syscalls.d ./Driver/src/syscalls.o ./Driver/src/syscalls.su ./Driver/src/sysmem.d ./Driver/src/sysmem.o ./Driver/src/sysmem.su
 
 .PHONY: clean-Driver-2f-src
 
